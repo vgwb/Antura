1e5de2b0112ba667986a325389dacaf3c678d349
Stefano Cecere <stefano.cecere@krur.com>
2019-02-18 12:29:18 +0100

    we don’t need to check updated from previous version

--- Assets/_app/_scripts/_Core/BootstrapScene.cs
+++ Assets/_app/_scripts/_Core/BootstrapScene.cs
@@ -18,17 +18,18 @@ namespace Antura.Scenes
             Debug.Log("BootstrapScene: Start()");
             // GlobalUI.ShowPauseMenu(false);
 
-            if (AppManager.I.AppSettingsManager.IsAppJustUpdatedFromOldVersion()) {
-                Debug.Log("BootstrapScene: Updating from Old version");
-                if (AppManager.I.AppSettings.SavedPlayers != null) {
-                    AppManager.I.PlayerProfileManager.DeleteAllPlayers();
-                    PanelAppUpdate.Init();
-                } else {
-                    GoToHomeScene();
-                }
-            } else {
-                GoToHomeScene();
-            }
+            //if (AppManager.I.AppSettingsManager.IsAppJustUpdatedFromOldVersion()) {
+            //    Debug.Log("BootstrapScene: Updating from Old version");
+            //    if (AppManager.I.AppSettings.SavedPlayers != null) {
+            //        AppManager.I.PlayerProfileManager.DeleteAllPlayers();
+            //        PanelAppUpdate.Init();
+            //    } else {
+            //        GoToHomeScene();
+            //    }
+            //} else {
+            //    GoToHomeScene();
+            //}
+            GoToHomeScene();
         }
 
         private void GoToHomeScene()
