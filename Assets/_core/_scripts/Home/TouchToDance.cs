ba9e8f2d1ae6347ef862a33278aa09754a8a6907
Michele <cat.piro@gmail.com>
2022-01-12 11:41:51 +0100

    [Home] Use only isolated forms (and play the letter name) for the LL close to the logo #222

--- Assets/_core/_scripts/Home/TouchToDance.cs
+++ Assets/_core/_scripts/Home/TouchToDance.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Antura.Audio;
 using Antura.Core;
+using Antura.Database;
 using Antura.Teacher;
 using UnityEngine;
 
@@ -16,10 +17,13 @@ namespace Antura.LivingLetters
 
             var letterFilters = new LetterFilters();
             letterFilters.excludeDiphthongs = true;
+            letterFilters.excludeDiacritics = AppManager.I.ContentEdition.PlayNameSoundWithForms ? LetterFilters.ExcludeDiacritics.All : LetterFilters.ExcludeDiacritics.None;
             var letter = AppManager.I.Teacher.GetRandomTestLetterLL(letterFilters, useMaxJourneyData: true);
             view.Init(letter);
 
-            AudioManager.I.PlayLetter(letter.Data, true);
+            var soundType = LetterDataSoundType.Phoneme;
+            if (AppManager.I.ContentEdition.PlayNameSoundWithForms) soundType = LetterDataSoundType.Name;
+            AudioManager.I.PlayLetter(letter.Data, true, soundType);
         }
     }
 }
