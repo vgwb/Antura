26d3e55360c13c399424491f761f5b658d87a306
Michele <cat.piro@gmail.com>
2022-07-26 15:34:26 +0200

    Added search function to minigame DB

--- Assets/_core/_scripts/Database/DatabaseObjects/MiniGameDatabase.cs
+++ Assets/_core/_scripts/Database/DatabaseObjects/MiniGameDatabase.cs
@@ -1,3 +1,10 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEditor;
+#endif
+
+using System;
+using DG.DeExtensions;
 using UnityEngine;
 
 namespace Antura.Database
@@ -9,5 +16,35 @@ namespace Antura.Database
     {
         [SerializeField]
         public MiniGameTable table;
+
+        public List<MiniGameData> ShownData = new List<MiniGameData>();
     }
+
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(MiniGameDatabase))]
+    public class MiniGameDatabaseEditor : Editor
+    {
+        private string filter;
+
+        public override void OnInspectorGUI()
+        {
+            filter = EditorGUILayout.TextField(filter);
+
+            serializedObject.Update();
+            var db = target as MiniGameDatabase;
+            db.ShownData.Clear();
+            foreach (var v in db.table.GetValuesTyped())
+            {
+                if (filter.IsNullOrEmpty() || v.CodeName.Contains(filter, StringComparison.OrdinalIgnoreCase))
+                {
+                    db.ShownData.Add(v);
+                }
+            }
+            var list = serializedObject.FindProperty("ShownData");
+            EditorGUI.PropertyField(GUILayoutUtility.GetRect(0f, 5000f), list, true);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+    #endif
 }
