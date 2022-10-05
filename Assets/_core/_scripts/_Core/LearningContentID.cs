b22267aed10dfc7577c4436ffe987ac5d8e894e8
Michele <cat.piro@gmail.com>
2022-10-05 12:27:08 +0200

    Fix renaming

--- Assets/_core/_scripts/_Core/LearningContentID.cs
+++ Assets/_core/_scripts/_Core/LearningContentID.cs
@@ -7,7 +7,7 @@
         LearnToRead_Dari = 1,
         LearnToRead_Pashto = 2,
         LearnToRead_Arabic = 20,
-        Learn_Arabic_Darija = 21,
+        LearnToRead_Arabic_Darija = 21,
         Learn_English = 30,
         Learn_French = 40,
         Learn_Italian = 50,
