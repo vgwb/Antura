57cb547c434201be35d3654c05e82eee0b7f77aa
Michele <cat.piro@gmail.com>
2022-10-05 12:24:31 +0200

    Renamed Learn_Arabic to LearnToRead_Arabic in LearningContentID

--- Assets/_core/_scripts/_Core/LearningContentID.cs
+++ Assets/_core/_scripts/_Core/LearningContentID.cs
@@ -6,7 +6,7 @@
 
         LearnToRead_Dari = 1,
         LearnToRead_Pashto = 2,
-        Learn_Arabic = 20,
+        LearnToRead_Arabic = 20,
         Learn_Arabic_Darija = 21,
         Learn_English = 30,
         Learn_French = 40,
