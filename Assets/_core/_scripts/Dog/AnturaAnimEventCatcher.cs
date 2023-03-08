0bd6eeef2f60b8c9aaa1fd55ae05fdc880059e93
Michele <cat.piro@gmail.com>
2023-03-08 22:42:37 +0100

    Anim event catcher. Anim updates for cat & dog.

---
+++ Assets/_core/_scripts/Dog/AnturaAnimEventCatcher.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Antura.Dog
+{
+    public class AnturaAnimEventCatcher : MonoBehaviour
+    {
+        void OnAnimationJumpGrab()
+        {
+            GetComponentInParent<AnturaAnimationController>().OnAnimationJumpGrab();
+        }
+    }
+}
