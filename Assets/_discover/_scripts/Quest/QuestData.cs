df59490735a3a017770fa6aa3d1ba57ec7dcda63
Stefano Cecere <stefano.cecere@krur.com>
2024-03-20 08:49:09 +0100

    Quest Data

---
+++ Assets/_discover/_scripts/Quest/QuestData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Antura.Minigames.DiscoverCountry
+{
+    [CreateAssetMenu(menuName = "Antura/Quest Data")]
+    public class QuestData : ScriptableObject
+    {
+        public string Title;
+    }
+}
---
+++ Assets/_discover/_scripts/Quest/QuestManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Antura.Teacher;
+using UnityEngine;
+
+namespace Antura.Minigames.DiscoverCountry
+{
+    public class QuestManager : MonoBehaviour
+    {
+        public QuestData Quest;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
