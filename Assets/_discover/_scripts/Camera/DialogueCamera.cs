2c565e73b9cf8b7754871bf2030e282aef931e20
Daniele Giardini <daniele.giardini@gmail.com>
2024-04-05 18:21:25 +0200

    UI : Dialogue layout setup + Camera changes

--- Assets/_discover/_scripts/Camera/DialogueCamera.cs
+++ Assets/_discover/_scripts/Camera/DialogueCamera.cs
@@ -1,14 +1,32 @@
-﻿using UnityEngine;
+﻿using Unity.Cinemachine;
+using UnityEngine;
 
 namespace Antura.Minigames.DiscoverCountry
 {
     public class DialogueCamera : AbstractCineCamera
     {
+        #region Serialized
+
+        [Range(0, -0.3f)]
+        [SerializeField] float composerScreenPositionXWChoices = -0.22f;
+
+        #endregion
+
+        float defComposerScreenPositionX;
+        CinemachinePositionComposer positionComposer;
+
+        void Awake()
+        {
+            positionComposer = CineMain.GetComponent<CinemachinePositionComposer>();
+            defComposerScreenPositionX = positionComposer.Composition.ScreenPosition.x;
+        }
+        
         #region Public Methods
 
-        public void SetTarget(Transform target)
+        public void SetTarget(Transform target, bool hasChoices = false)
         {
             cineMain.Target.TrackingTarget = target;
+            positionComposer.Composition.ScreenPosition.x = hasChoices ? composerScreenPositionXWChoices : defComposerScreenPositionX;
         }
 
         #endregion
