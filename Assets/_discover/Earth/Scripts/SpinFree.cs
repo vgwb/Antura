41ec4a52096db0d69f92437bcaa54c20eee1bc94
Stefano Cecere <stefano.cecere@krur.com>
2024-04-07 00:45:42 +0200

    Earth scene

---
+++ Assets/_discover/Earth/Scripts/SpinFree.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Spin the object at a specified speed
+/// </summary>
+public class SpinFree : MonoBehaviour {
+	[Tooltip("Spin: Yes or No")]
+	public bool spin;
+	[Tooltip("Spin the parent object instead of the object this script is attached to")]
+	public bool spinParent;
+	public float speed = 10f;
+
+	[HideInInspector]
+	public bool clockwise = true;
+	[HideInInspector]
+	public float direction = 1f;
+	[HideInInspector]
+	public float directionChangeSpeed = 2f;
+
+	// Update is called once per frame
+	void Update() {
+		if (direction < 1f) {
+			direction += Time.deltaTime / (directionChangeSpeed / 2);
+		}
+
+		if (spin) {
+			if (clockwise) {
+				if (spinParent)
+					transform.parent.transform.Rotate(Vector3.up, (speed * direction) * Time.deltaTime);
+				else
+					transform.Rotate(Vector3.up, (speed * direction) * Time.deltaTime);
+			} else {
+				if (spinParent)
+					transform.parent.transform.Rotate(-Vector3.up, (speed * direction) * Time.deltaTime);
+				else
+					transform.Rotate(-Vector3.up, (speed * direction) * Time.deltaTime);
+			}
+		}
+	}
+}
