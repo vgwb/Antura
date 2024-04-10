6b802a506611bb0e3c5864e032ff90f76a632d36
Stefano Cecere <stefano.cecere@krur.com>
2024-04-10 11:35:03 +0200

    France Map

--- Assets/_discover/Earth/Scripts/SpinObject.cs
+++ Assets/_discover/Earth/Scripts/SpinObject.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 namespace Antura.Minigames.DiscoverCountry
 {
@@ -12,38 +11,21 @@ namespace Antura.Minigames.DiscoverCountry
         public bool spin;
         [Tooltip("Spin the parent object instead of the object this script is attached to")]
         public bool spinParent;
+
         public float speed = 10f;
 
-        [HideInInspector]
-        public bool clockwise = true;
-        [HideInInspector]
-        public float direction = 1f;
-        [HideInInspector]
-        public float directionChangeSpeed = 2f;
-
-        // Update is called once per frame
         void Update()
         {
-            if (direction < 1f)
-            {
-                direction += Time.deltaTime / (directionChangeSpeed / 2);
-            }
 
             if (spin)
             {
-                if (clockwise)
+                if (spinParent)
                 {
-                    if (spinParent)
-                        transform.parent.transform.Rotate(Vector3.up, (speed * direction) * Time.deltaTime);
-                    else
-                        transform.Rotate(Vector3.up, (speed * direction) * Time.deltaTime);
+                    transform.parent.transform.Rotate(Vector3.up, speed * Time.deltaTime);
                 }
                 else
                 {
-                    if (spinParent)
-                        transform.parent.transform.Rotate(-Vector3.up, (speed * direction) * Time.deltaTime);
-                    else
-                        transform.Rotate(-Vector3.up, (speed * direction) * Time.deltaTime);
+                    transform.Rotate(Vector3.up, speed * Time.deltaTime);
                 }
             }
         }
