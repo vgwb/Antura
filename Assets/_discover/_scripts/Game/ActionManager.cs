85387931dbce555795ab73386571af34a5258cd2
Stefano Cecere <stefano.cecere@krur.com>
2024-06-18 09:25:11 +0200

    beams

--- Assets/_discover/_scripts/Game/ActionManager.cs
+++ Assets/_discover/_scripts/Game/ActionManager.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActionManager : MonoBehaviour
+namespace Antura.Minigames.DiscoverCountry
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ActionManager : MonoBehaviour
     {
-        
-    }
+        public static ActionManager I;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public GameObject Beam_Eiffel;
+        public GameObject Beam_NotreDame;
+        public GameObject Beam_Louvre;
+        public GameObject Beam_Baguette;
+
+        void Awake()
+        {
+            if (I == null)
+            {
+                I = this;
+            }
+            else
+            {
+                Debug.Log("ActionManager DUPLICATED!");
+                Destroy(gameObject);
+            }
+        }
+
+        void Start()
+        {
+            Beam_Eiffel.SetActive(false);
+        }
+
+        public void ShowNextTarget(string targetName)
+        {
+            Beam_Eiffel.SetActive(true);
+        }
+
+
     }
 }
