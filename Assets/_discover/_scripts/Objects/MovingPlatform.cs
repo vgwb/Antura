f692d95cf3925ffd381c387b5f5a7386b6a49c4d
Catman87 <cat.piro@gmail.com>
2024-06-21 14:10:32 +0200

    Added delay to moving platform

--- Assets/_discover/_scripts/Objects/MovingPlatform.cs
+++ Assets/_discover/_scripts/Objects/MovingPlatform.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Antura.Minigames.DiscoverCountry
 {
@@ -9,7 +10,9 @@ namespace Antura.Minigames.DiscoverCountry
         public bool IsActivated;
         public float speed;
         public float distance = 2.0f;
+        public float pauseDelay = 1f;
 
+        private float currentPause = 0f;
         private Vector3 startingPosition;
         private bool movingUp = true;
 
@@ -22,16 +25,24 @@ namespace Antura.Minigames.DiscoverCountry
         {
             if (IsActivated)
             {
+                if (currentPause > 0f)
+                {
+                    currentPause -= Time.deltaTime;
+                    return;
+                }
+
                 float direction = movingUp ? 1f : -1f;
                 transform.position += Vector3.up * direction * speed * Time.deltaTime;
 
                 if (movingUp && transform.position.y >= startingPosition.y + distance)
                 {
                     movingUp = false;
+                    currentPause = pauseDelay;
                 }
                 else if (!movingUp && transform.position.y <= startingPosition.y)
                 {
                     movingUp = true;
+                    currentPause = pauseDelay;
                 }
             }
         }
