c0985c190b2af5462303f002db7a37019569566b
Stefano Cecere <stefano.cecere@krur.com>
2025-01-08 19:12:49 +0100

    Action Manager

--- Assets/_discover/_scripts/Game/ActionData.cs
+++ Assets/_discover/_scripts/Game/ActionData.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 namespace Antura.Minigames.DiscoverCountry
 {
+    public enum ActionType
+    {
+        UnityAction = 1,
+        Area = 2,
+    }
 
     [System.Serializable]
     public class ActionData
     {
+        public ActionType Type;
         public string ActionCode;
         public GameObject mainObject;
         public GameObject Area;
+        public GameObject Beam;
+        public GameObject Target;
     }
 }
--- Assets/_discover/_scripts/Game/ActionManager.cs
+++ Assets/_discover/_scripts/Game/ActionManager.cs
@@ -2,6 +2,8 @@ using Antura.Audio;
 using Antura.Minigames.DiscoverCountry.Interaction;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 namespace Antura.Minigames.DiscoverCountry
@@ -11,27 +13,9 @@ namespace Antura.Minigames.DiscoverCountry
         public static ActionManager I;
 
         public ActionData[] Actions;
-        public Transform Target_TutorialTreasure;
-        public Transform Target_Eiffel;
-        public Transform Target_NotreDame;
-        public Transform Target_Louvre;
-        public Transform Target_Bakery;
-        public Transform Target_Seine;
+
+        [Header("Specific")]
         public Transform Target_AnturaLocation;
-
-        public GameObject Beam_Tutorial;
-        public GameObject Beam_Eiffel;
-        public GameObject Beam_NotreDame;
-        public GameObject Beam_Louvre;
-        public GameObject Beam_Bakery;
-        public GameObject Beam_Seine;
-
-        public GameObject Area_Tutorial;
-        public GameObject Area_Eiffel;
-        public GameObject Area_NotreDame;
-        public GameObject Area_Louvre;
-        public GameObject Area_Bakery;
-        public GameObject Area_Seine;
 
         public GameObject TutorialElevator;
         public GameObject Eiffel_Guide;
@@ -62,100 +46,74 @@ namespace Antura.Minigames.DiscoverCountry
         {
             Player = GameObject.FindWithTag("Player");
 
-            Area_Tutorial.SetActive(false);
-            Area_Eiffel.SetActive(false);
-            Area_NotreDame.SetActive(false);
-            Area_Louvre.SetActive(false);
-            Area_Bakery.SetActive(false);
-            Area_Seine.SetActive(false);
+            foreach (var action in Actions)
+            {
+                if (action.Type == ActionType.Area)
+                {
+                    action.Area.SetActive(false);
+                    action.Beam.SetActive(false);
+                }
+            }
 
-            Beam_Tutorial.SetActive(false);
-            Beam_Eiffel.SetActive(false);
-            Beam_NotreDame.SetActive(false);
-            Beam_Louvre.SetActive(false);
-            Beam_Bakery.SetActive(false);
-            Beam_Seine.SetActive(false);
             Target_AnturaLocation = null;
-
-            Eiffel_Guide.SetActive(true);
-            NotreDame_Major.SetActive(true);
-
             AnturaDog.SetActive(false);
         }
 
-        public void CameraShowTarget(string targetName)
+        private ActionData GetActionDataByCode(string actionCode)
         {
-            switch (targetName)
-            {
-                case "tutorial":
-                    InteractionManager.I.FocusCameraOn(Target_TutorialTreasure);
-                    Target_AnturaLocation = null;
-                    break;
-                case "toureiffel":
-                    InteractionManager.I.FocusCameraOn(Target_Eiffel);
-                    Beam_Eiffel.SetActive(true);
-                    Target_AnturaLocation = Beam_Eiffel.transform;
-                    break;
-                case "notredame":
-                    InteractionManager.I.FocusCameraOn(Target_NotreDame);
-                    Beam_NotreDame.SetActive(true);
-                    Target_AnturaLocation = Beam_NotreDame.transform;
-                    break;
-                case "louvre":
-                    InteractionManager.I.FocusCameraOn(Target_Louvre);
-                    Beam_Louvre.SetActive(true);
-                    Target_AnturaLocation = Beam_Louvre.transform;
-                    break;
-                case "bakery":
-                    InteractionManager.I.FocusCameraOn(Target_Bakery);
-                    Beam_Bakery.SetActive(true);
-                    Target_AnturaLocation = Beam_Bakery.transform;
-                    break;
-                case "sein":
-                    InteractionManager.I.FocusCameraOn(Target_Seine);
-                    Beam_Seine.SetActive(true);
-                    Target_AnturaLocation = Beam_Seine.transform;
-                    break;
-                default:
-                    break;
-            }
-            if (Target_AnturaLocation != null)
-                InteractionManager.I.ActivateWorldTargetIcon(true, Target_AnturaLocation);
+            return Actions.FirstOrDefault(action => action.ActionCode == actionCode);
         }
 
-        public void ShowNextTarget(string targetName)
+        public void CameraShowTarget(string targetArea)
         {
+            Debug.Log("CameraShowTarget targetArea:" + targetArea);
 
+
+            var actionData = GetActionDataByCode(targetArea);
+
+            Debug.Log("CameraShowTarget ActionCode:" + actionData.ActionCode);
+            InteractionManager.I.FocusCameraOn(actionData.Target.transform);
+            actionData.Beam.SetActive(true);
+            Target_AnturaLocation = actionData.Target.transform;
+
+            if (Target_AnturaLocation != null)
+            {
+                InteractionManager.I.ActivateWorldTargetIcon(true, Target_AnturaLocation);
+            }
+        }
+
+        private void ActivateArea(string targetArea)
+        {
+            var actionData = GetActionDataByCode(targetArea);
+            actionData.Area.SetActive(true);
         }
 
         public void ResolveAction(string action)
         {
             Debug.Log("ResolveAction" + action);
+
             switch (action)
             {
                 case "updatecoins":
                     QuestManager.I.UpateCoinsCounter();
                     break;
                 case "area_tutorial":
-                    Area_Tutorial.SetActive(true);
-                    TutorialElevator.GetComponent<MovingPlatform>().Activate(true);
+                    ActivateArea("tutorial");
                     break;
                 case "area_eiffel":
-                    Area_Eiffel.SetActive(true);
-                    Eiffel_Guide.SetActive(false);
+                    ActivateArea("eiffel");
                     break;
                 case "area_notredame":
-                    Area_NotreDame.SetActive(true);
-                    NotreDame_Major.SetActive(false);
+                    ActivateArea("notredame");
                     break;
                 case "area_louvre":
-                    Area_Louvre.SetActive(true);
+                    ActivateArea("louvre");
                     break;
                 case "area_bakery":
-                    Area_Bakery.SetActive(true);
+                    ActivateArea("bakery");
                     break;
                 case "area_seine":
-                    Area_Seine.SetActive(true);
+                    ActivateArea("seine");
                     break;
                 case "game_end":
                     WinFx.SetActive(true);
@@ -168,7 +126,7 @@ namespace Antura.Minigames.DiscoverCountry
                     Player.GetComponent<EdPlayer>().SpawnToNewLocation(Spawn_Louvre_Exit);
                     break;
                 case "louvre_enter":
-                    Area_Louvre.SetActive(true);
+                    ActivateArea("louvre");
                     Player.GetComponent<EdPlayer>().SpawnToNewLocation(Spawn_Louvre_Enter);
                     break;
             }
