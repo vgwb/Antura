a240ce6889114041c0337f82ef2ed3f63d3e5960
Stefano Cecere <stefano.cecere@krur.com>
2025-02-09 19:12:21 +0100

    Debug function to see all unused permalinks in scene

--- Assets/_discover/Homer/HomerParser/Runtime/HomerConfig.cs
+++ Assets/_discover/Homer/HomerParser/Runtime/HomerConfig.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 using UnityEngine;
+using DG.DeInspektor.Attributes;
+using Newtonsoft.Json;
+using Antura.Minigames.DiscoverCountry;
 
 namespace Homer
 {
@@ -8,9 +13,11 @@ namespace Homer
     {
         //THIS MUST BE CONFIGURED BY HAND: DRAG Homer/ProjectData/Homer(.json) HERE!!!
         public TextAsset Homer;
-        
+
+        public HomerFlowSlugs.FlowSlug SelectedFlow;
+
         public static HomerConfig I;
-        
+
         void Awake()
         {
             if (I == null)
@@ -24,11 +31,54 @@ namespace Homer
                 Destroy(gameObject);
             }
         }
-        
-        void Start()
+
+        [DeMethodButton(mode = DeButtonMode.Default)]
+        void ListAllPermalinks()
         {
-        
+            Debug.Log("Permalinks of Quest " + SelectedFlow);
+            HomerProject project = JsonConvert.DeserializeObject<HomerProject>(Homer.text);
+            var permalinks = GetPermalinksBySlug(project._flows, SelectedFlow.ToString());
+
+            foreach (string permalink in permalinks)
+            {
+                Debug.Log(permalink + "\n");
+            }
         }
+        [DeMethodButton(mode = DeButtonMode.Default)]
+        public void CheckNotUsedPermalinks()
+        {
+            HomerProject project = JsonConvert.DeserializeObject<HomerProject>(Homer.text);
+            var permalinks = GetPermalinksBySlug(project._flows, SelectedFlow.ToString());
+
+            // Find all Interactable components in the scene
+            Interactable[] interactables = FindObjectsOfType<Interactable>();
+
+            // Check if any NodePermalink in Interactable matches the permalinks
+            var usedPermalinks = interactables.Select(i => i.NodePermalink).ToHashSet();
+            var notUsedPermalinks = permalinks.Where(p => !usedPermalinks.Contains(p)).ToArray();
+
+            var output = "";
+            foreach (var permalink in notUsedPermalinks)
+            {
+                output += permalink + "\n";
+            }
+            Debug.Log(notUsedPermalinks.Count() + " not used permalinks of Quest " + SelectedFlow + "\n\n" + output);
+        }
+
+        private string[] GetPermalinksBySlug(HomerFlow[] _flows, string slug)
+        {
+            var flow = _flows.FirstOrDefault(f => f._slug == slug);
+            if (flow == null)
+            {
+                return new string[0];
+            }
+            return flow._nodes
+                .Where(node => !string.IsNullOrEmpty(node._permalink))
+                .Select(node => node._permalink)
+                .ToArray();
+        }
+
+
 
     }
 }
