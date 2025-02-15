085fd8096022b0dde8aeea45043f5c8e372d38eb
Stefano Cecere <stefano.cecere@krur.com>
2025-02-15 18:16:23 +0100

    rotating Items in Map

--- Assets/_discover/_scripts/UI/Map/PlayerMapIcon.cs
+++ Assets/_discover/_scripts/UI/Map/PlayerMapIcon.cs
@@ -6,12 +6,21 @@ namespace Antura.Minigames.DiscoverCountry
     public class PlayerMapIcon : AbstractMapIcon
     {
         public override bool IsEnabled => true;
-        
+        public bool Rotate;
+
         #region Methods
 
         protected override Vector3 GetPosition()
         {
             return InteractionManager.I.player.transform.position;
+        }
+
+        void Update()
+        {
+            if (Rotate)
+            {
+                transform.Rotate(Vector3.up * 10 * Time.fixedDeltaTime);
+            }
         }
 
         #endregion
