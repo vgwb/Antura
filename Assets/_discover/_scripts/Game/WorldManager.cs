0935978ff50e31841b447e3acd650f89a8f39504
Stefano Cecere <stefano.cecere@krur.com>
2025-08-07 06:59:50 +0200

    reorganize scenes with WorldManager and compacted GameManager

---
+++ Assets/_discover/_scripts/Game/WorldManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Antura.Discover
+{
+    public class WorldManager : MonoBehaviour
+    {
+        public GameObject[] environmentPrefabs; // Array of environment prefabs to choose from
+        public Transform environmentParent; // Parent transform to hold the instantiated environment
+        void Start()
+        {
+
+        }
+
+
+    }
+}
