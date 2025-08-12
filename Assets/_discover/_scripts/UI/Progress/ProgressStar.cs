3d62dac89d50c554121d5af719df7e457a3bcc6e
Stefano Cecere <stefano.cecere@krur.com>
2025-08-12 19:33:14 +0200

    prefab and tune position of ProgressDisplay

--- Assets/_discover/_scripts/UI/Progress/ProgressStar.cs
+++ Assets/_discover/_scripts/UI/Progress/ProgressStar.cs
@@ -1,4 +1,5 @@
-﻿using DG.DeInspektor.Attributes;
+﻿using System.Drawing;
+using DG.DeInspektor.Attributes;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,10 +11,13 @@ namespace Antura.Discover
         #region Serialized
 
         [DeEmptyAlert]
-        [SerializeField] Image star;
+        [SerializeField] UnityEngine.UI.Image star;
+
+        [DeEmptyAlert]
+        [SerializeField] GameObject line;
 
         #endregion
-        
+
         public bool Achieved { get; private set; }
         public int MinRequiredScore { get; private set; }
 
@@ -35,22 +39,31 @@ namespace Antura.Discover
             if (achievedTween == null)
             {
                 achievedTween = DOTween.Sequence().SetAutoKill(false).Pause()
-                    .Join(star.DOColor(Color.white, 0.1f).From(Color.black).SetEase(Ease.Linear))
+                    .Join(star.DOColor(UnityEngine.Color.white, 0.1f).From(new UnityEngine.Color(0, 0, 0, 0.2f)).SetEase(Ease.Linear))
                     .Join(star.transform.DOPunchScale(Vector3.one * 1.2f, 0.5f))
                     .Insert(0.25f, star.transform.DOLocalRotate(new Vector3(0, 0, 360), 0.5f, RotateMode.FastBeyond360).From(true));
             }
-            
+
             MinRequiredScore = minRequiredScore;
             Achieve(false);
         }
 
         public void Achieve(bool achieve)
         {
-            if (Achieved == achieve) return;
+            if (Achieved == achieve)
+                return;
 
             Achieved = achieve;
-            if (Achieved) achievedTween.Restart();
-            else achievedTween.Rewind();
+            if (Achieved)
+            {
+                line.SetActive(true);
+                achievedTween.Restart();
+            }
+            else
+            {
+                line.SetActive(false);
+                achievedTween.Rewind();
+            }
         }
 
         #endregion
