9fd6d06c5c9f49b23f714e2475ca5fbd6cd0a12d
Stefano Cecere <stefano.cecere@krur.com>
2025-08-12 14:36:20 +0200

    rework Task Data for global achievements

--- Assets/_discover/_scripts/Data/TaskData.cs
+++ Assets/_discover/_scripts/Data/TaskData.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
-using DG.DeInspektor.Attributes;
+using UnityEngine.Localization;
 using System;
 
 namespace Antura.Discover
@@ -13,33 +12,35 @@ namespace Antura.Discover
         Collect = 1,
         Reach = 2,
         Interact = 3,
+        Performance = 4
     }
 
     [CreateAssetMenu(fileName = "TaskData", menuName = "Antura/Discover/Task")]
     public class TaskData : ScriptableObject
     {
-        [Tooltip("Task Code used in a Node to activate it")]
-        public string Code;
+        [Header("Identity")]
+        [Tooltip("Unique, stable ID. lowercase, Never change after shipping.")]
+        public string Id;
+
+        [Tooltip("Global or specific to a country?")]
+        public Countries Country;
+
         public TaskType Type;
+
+        [Header("Content")]
+        public LocalizedString Title;
+
+        [Tooltip("Optional description")]
+        public LocalizedString Description;
+
+        [Header("Achievements")]
+
         [Tooltip("Total or per item is collect Task")]
         public int ProgressPoints;
 
-        [Tooltip("Optional permalink of the Node with the mission")]
-        public string NodeDescription;
+        [Tooltip("Gems gained")]
+        public int Gems;
 
-        [Tooltip("Optional permalink of the Node activated if success task")]
-        public string NodeSuccess;
-
-        [Tooltip("Optional permalink of the Node activated if fail task")]
-        public string NodeFail;
-
-        [Tooltip("If Collect, how many items to collect?")]
-        public int ItemCount;
-        [Tooltip("If Collect, what tag to count?")]
-        public string ItemTag;
-
-        [Tooltip("Optional timer for the task, in seconds")]
-        public int TimerSeconds = 0;
 
     }
 }
