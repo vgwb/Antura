7a6a617367ebfc52e4bb75a9a06a3ed4e9c09a20
Stefano Cecere <stefano.cecere@krur.com>
2025-09-28 20:18:13 +0200

    introducing DOMUS quest. the playground and player Space. TBD soon

---
+++ Assets/_discover/_scripts/Domus/DomusManager.cs
@@ -0,0 +1,16 @@
+using Antura.Core;
+using Antura.Utilities;
+using AdventurEd;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Antura.Discover.Domus
+{
+    /// <summary>
+    /// Manages the Domus environment and its interactions.
+    /// </summary>
+    public class DomusManager : SingletonMonoBehaviour<DomusManager>
+    {
+    }
+}
